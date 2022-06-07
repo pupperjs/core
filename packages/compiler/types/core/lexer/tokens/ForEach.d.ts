a6553f9d7a6b723ca52bf6bbb3ede948ebef26fe
Matheus Giovani <teusgiovani@gmail.com>
2022-06-06 23:33:34 -0300

    removes the directory types from the dist

--- packages/compiler/types/core/lexer/tokens/ForEach.d.ts
+++
@@ -1,5 +0,0 @@
-import { PugNode } from "../../Plugin";
-import Token from "../Token";
-export default class ForEach extends Token {
-    parse(nodes: PugNode[]): PugNode[];
-}
--- packages/compiler/types/core/lexer/tokens/PupperToAlpine.d.ts
+++
@@ -1,6 +0,0 @@
-import { PugToken } from "../../Plugin";
-import Token from "../Token";
-export default class PupperToAlpine extends Token {
-    static Directives: Record<string, string>;
-    lex(tokens: PugToken[]): PugToken[];
-}
